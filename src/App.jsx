18123ed2de8e598bfa65fa51c608f212393b2a27
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-05-18 10:27:56 +0200

    Added a context for accessing a socket.io instance.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import GamePage from "./pages/GamePage";
 import Container from "react-bootstrap/Container";
 import { Routes, Route } from "react-router-dom";
+import SocketContext from "./contexts/SocketContext";
 
 function App() {
   return (
     <div id="App">
-      <Container className="py-3">
-        <Routes>
-          <Route path="/gameboard" element={<GamePage />} />
-        </Routes>
-      </Container>
+      <SocketContext>
+        <Container className="py-3">
+          <Routes>
+            <Route path="/gameboard" element={<GamePage />} />
+          </Routes>
+        </Container>
+      </SocketContext>
     </div>
   );
 }
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSocketContext } from "../contexts/SocketContext";
 
 export default function GamePage() {
+  const socket = useSocketContext()
+  console.log(socket)
   return (
     <>
       <h1>Game page!</h1>
