2ce94341d61992c5a576233b3e043456d1f01789
Malin Olsson <miragia.solinde@gmail.com>
2022-05-18 14:36:58 +0200

    Updated startpage

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -12,40 +12,39 @@ const StartPage = () => {
 
             <div className='form-box'>
 
-            <h1>BattleShips</h1>
+                <h1>BattleShips</h1>
 
-            <Form className='text-center'>
+                <Form className='text-center'>
 
-                <Form.Group className="group-form">
+                    <Form.Group className="group-form">
 
-                    <Form.Control className='form-input' id="form-input-name" type="text" placeholder="Name, please." />
+                        <Form.Control className='form-input' id="form-input-name" type="text" placeholder="Name, please." />
 
-                    <Form.Text>
+                        <Form.Text>
 
-                        Please, enter your name here.
+                            Please, enter your name here.
 
-                    </Form.Text>
+                        </Form.Text>
 
-                
-                </Form.Group>
+                    </Form.Group>
 
-                <Button className="join-button" id="join-button-id" variant="info" as={Link} to="/GamePage" onclick="">Start Game</Button>
+                    <Button className="join-button" id="join-button-id" variant="info" as={Link} to="/GamePage" onclick="">Start Game</Button>
 
-            </Form>
+                </Form>
 
             
 
             </div>
 
-        <div className="rules">
+            <div className="rules">
 
-            <h2>How to play BattleShips!</h2>
+                <h2>How to play BattleShips!</h2>
 
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt praesentium accusamus excepturi esse, iste architecto aspernatur alias ullam numquam temporibus asperiores cumque ab voluptatum illo, veniam magni porro ex. Minus.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt praesentium accusamus excepturi esse, iste architecto aspernatur alias ullam numquam temporibus asperiores cumque ab voluptatum illo, veniam magni porro ex. Minus.</p>
 
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ex culpa soluta labore aut aperiam eos ipsum repellendus delectus neque. Aspernatur consequatur exercitationem iste unde a debitis blanditiis maiores suscipit?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ex culpa soluta labore aut aperiam eos ipsum repellendus delectus neque. Aspernatur consequatur exercitationem iste unde a debitis blanditiis maiores suscipit?</p>
 
-        </div>
+            </div>
         </div>
 
         </>
