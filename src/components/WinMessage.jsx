80f179f515dd9c280dc3b4e1a1bd0ba1a239f8d7
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-05-31 11:19:20 +0200

    Imported the bootstrap components used.

--- src/components/WinMessage.jsx
+++ src/components/WinMessage.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import {Alert, Button} from 'react-bootstrap'
 
 export default function WinMessage() {
   return (
     <Alert variant="success">
-     <h2>Congrats! You won this round!</h2>
+      <h2>Congrats! You won this round!</h2>
 
-        <hr />
+      <hr />
 
-        <Button
+      <Button
         className="play-again-button"
         id="play-again"
         variant="info"
         onclick="">
-            Play Again?
-        </Button>
+        Play Again?
+      </Button>
 
     </Alert>
   )
