b9125f884d0f8e69cb7c03686fd04c34acb59881
Josefine Ahlstedt <josefine.ahlstedt@gmail.com>
2022-06-01 12:58:22 +0200

    joinbutton is now hidden when a game is in progress

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,52 +1,47 @@
-import React, { useState, useEffect } from "react"
-import { useSocketContext } from "../contexts/SocketContext"
-import Gameboard from "../components/Gameboard"
-import Waves from "../components/Waves"
-import Header from "../components/Header"
-import WinMessage from "../components/WinMessage"
-import { Button } from "react-bootstrap"
-import OpponentGameBoard from "../components/OpponentGameboard"
-import Alert from "react-bootstrap/Alert"
-import LoosingMessage from "../components/LoosingMessage"
-
+import React, { useState, useEffect } from "react";
+import { useSocketContext } from "../contexts/SocketContext";
+import Gameboard from "../components/Gameboard";
+import Waves from "../components/Waves";
+import Header from "../components/Header";
+import WinMessage from "../components/WinMessage";
+import { Button } from "react-bootstrap";
+import OpponentGameBoard from "../components/OpponentGameboard";
+import Alert from "react-bootstrap/Alert";
+import LoosingMessage from "../components/LoosingMessage";
 
 export default function GamePage() {
-
   // get socket from the socket context.
-  const socket = useSocketContext()
+  const socket = useSocketContext();
 
   // States to control wether a button can be pressed and to display messages.
-  const [waitingForGame, setWaitingForGame] = useState(false)
-  const [gameFound, setGameFound] = useState(false)
-  const [gameInProgress, setGameInProgress] = useState(false)
+  const [waitingForGame, setWaitingForGame] = useState(false);
+  const [gameFound, setGameFound] = useState(false);
+  const [gameInProgress, setGameInProgress] = useState(false);
 
   // Tell the server that the user wants to join a game
   const joinGame = () => {
-
-    
-    setGameFound(false)
-    setWaitingForGame(true)
-    setLose(false)
-    setWin(false)
-    socket.emit("joinGame")
-
+    setGameFound(false);
+    setWaitingForGame(true);
+    setLose(false);
+    setWin(false);
+    socket.emit("joinGame");
   };
 
   // Start listening on the following emits
   useEffect(() => {
     socket.on("connected", (text) => {
-      console.log(text)
+      console.log(text);
     });
 
     socket.on("HiRoom", () => {
-      console.log("Server said hi to your room")
+      console.log("Server said hi to your room");
     });
 
     socket.on("gameFound", () => {
-      setWaitingForGame(false)
-      setGameFound(true)
-      setGameInProgress(true)
-      setGameFound(false)
+      setWaitingForGame(false);
+      setGameFound(true);
+      setGameInProgress(true);
+      setGameFound(false);
     });
 
     socket.on("userLeft", (message) => {
@@ -79,65 +74,71 @@ export default function GamePage() {
     "J",
   ]);
 
-  const [alert, setAlert] = useState("success")
-  const [message, setMessage] = useState("Yay It's your turn!")
+  const [alert, setAlert] = useState("success");
+  const [message, setMessage] = useState("Yay It's your turn!");
 
-  const ref = ["", "A", "B", "C", "D", "F", "G", "H", "I", "J", "K"]
+  const ref = ["", "A", "B", "C", "D", "F", "G", "H", "I", "J", "K"];
 
-  const [flag, setFlag] = useState()
+  const [flag, setFlag] = useState();
 
-  const [win, setWin] = useState(false)
-  const [lose, setLose] = useState(false)
+  const [win, setWin] = useState(false);
+  const [lose, setLose] = useState(false);
 
-  const handleSetLose= () => {
-    setLose(true)
-    
-  }
+  const handleSetLose = () => {
+    setLose(true);
+  };
 
   function changeFlag(boolean, string, message) {
-    setFlag(boolean)
-    setAlert(string)
-    setMessage(message)
+    setFlag(boolean);
+    setAlert(string);
+    setMessage(message);
   }
 
   useEffect(() => {
     socket.on("playerTurn", (id) => {
       if (socket.id === id) {
         setFlag(false);
-        console.log("You get to start!!!", flag)
-        setMessage("Yay It's your turn!")
-        setAlert("success")
+        console.log("You get to start!!!", flag);
+        setMessage("Yay It's your turn!");
+        setAlert("success");
       } else {
-        setFlag(true)
-        console.log("Aw it's the other players turn...", flag)
-        setMessage("Aw it's the other players turn...")
-        setAlert("danger")
+        setFlag(true);
+        console.log("Aw it's the other players turn...", flag);
+        setMessage("Aw it's the other players turn...");
+        setAlert("danger");
       }
-    })
+    });
 
     socket.on("changeTurn", (msg) => {
-      console.log(msg)
-      setFlag(false)
-      setAlert("success")
-      setMessage("Yay It's your turn!")
-    })
+      console.log(msg);
+      setFlag(false);
+      setAlert("success");
+      setMessage("Yay It's your turn!");
+    });
 
-    socket.on('win', () => {
-      console.log("Congratulations, you won!")
-      setWin(true)
-    })
-    socket.on('matchIsOver', () => {
-      console.log("The match is over")
-      setGameInProgress(false)
-    })
-  }, [])
+    socket.on("win", () => {
+      console.log("Congratulations, you won!");
+      setWin(true);
+    });
+    socket.on("matchIsOver", () => {
+      console.log("The match is over");
+      setGameInProgress(false);
+    });
+  }, []);
 
   return (
     <>
       <Header />
 
       <div className="text-center">
-        <Button className="w-auto joinGameButton" size="lg" disabled={waitingForGame} onClick={joinGame}>
+        <Button
+          className={`w-auto joinGameButton ${
+            gameInProgress === true ? "d-none" : ""
+          }`}
+          size="lg"
+          disabled={waitingForGame}
+          onClick={joinGame}
+        >
           Join game
         </Button>
       </div>
@@ -157,7 +158,13 @@ export default function GamePage() {
             <Alert key={alert} variant={alert}>
               {message}
             </Alert>
-            <Gameboard handleSetLose={handleSetLose} rows={row} columns={column} refs={ref} flagga={flag} />
+            <Gameboard
+              handleSetLose={handleSetLose}
+              rows={row}
+              columns={column}
+              refs={ref}
+              flagga={flag}
+            />
           </div>
 
           {/* Second gameboard */}
@@ -172,7 +179,6 @@ export default function GamePage() {
       )}
 
       <Waves />
-
     </>
-  )
+  );
 }
